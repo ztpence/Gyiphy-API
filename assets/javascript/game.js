07085e8c121c91739c0eb8866da42084ae7db814
Zane P <ztpence@gmail.com>
2018-03-09 22:19:23 -0700

    pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// May need a document.ready function or something that loads the first set of buttons and page
+//need an array of starting items or themes
+//Need to create buttons for items --- look at class examples
+//need to create onclick function for array items that are displayed on openning page
+//need to create onclick functio to create new item buttons and a .val .trim method for text
+// need to create ajax json function for giphy data to be returned to page
+//need to create a function for pausing and starting video clips
+// 
